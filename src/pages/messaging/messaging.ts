7fc709b93e845621aaac3d1b83c6f941c5d336b8
Ryan Scovill <ryans797@gmail.com>
2017-09-16 22:32:13 -0700

    message fixes

--- src/pages/messaging/messaging.ts
+++ src/pages/messaging/messaging.ts
@@ -7,11 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class MessagesPage {
 
+  private _messages: Array<any>;
+
   constructor(public navCtrl: NavController) {
-
+     this._messages = JSON.parse('[{"name":"jack", "message":"Hey, Im Jack, want to hit up a starbucks sometime?"},{"name":"john", "message":"Message"},{"name":"joe", "message":"Message"}]');
   }
 
-  public get messages(): object {
-    return JSON.parse('[{"name":"jack", "message":"Hey, Im Jack, want to hit up a starbucks sometime?"},{"name":"john", "message":"Message"},{"name":"joe", "message":"Message"}]');
+  public getMessages(): Array<any> {
+    return this._messages;
   }
+
+  public addMessage(name: string, message: string): void {
+    this._messages.push({name: name, message: message});
+  }
+
 }
