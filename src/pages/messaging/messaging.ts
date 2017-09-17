ab24f84267d07b06722d0356cd6a38c0912ff830
Ryan Scovill <ryans797@gmail.com>
2017-09-16 20:54:14 -0700

    Add message content

--- src/pages/messaging/messaging.ts
+++ src/pages/messaging/messaging.ts
@@ -10,4 +10,8 @@ export class MessagesPage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  public get messages(): object {
+    return JSON.parse('[{"name":"jack", "message":"Hey, Im Jack, want to hit up a starbucks sometime?"},{"name":"john", "message":"Message"},{"name":"joe", "message":"Message"}]');
+  }
 }
